875ae0a59bc026227bb746c334287515f217fd97
boringuser <eric.c.williams@gmail.com>
2014-02-09 16:47:47 -0800

    Initial check-in

---
+++ MvcMovie/MvcMovie/Controllers/ReportController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class ReportController : Controller
+    {
+        private MovieDBContext db = new MovieDBContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private ActionResult GetChart(IEnumerable<string> xValues, IEnumerable<int> yValues)
+        {
+            var chart = new Chart(width: 300, height: 200)
+                .AddSeries(
+                    chartType: "bar",
+                    xValue: xValues,
+                    yValues: yValues)
+                .GetBytes("png");
+
+            return File(chart, "image/bytes");
+        }
+
+        public ActionResult ChartByGenre()
+        {
+            var query = from m in db.Movies
+                group m by m.Genre
+                into grp
+                select new
+                {
+                    Genre = grp.Key,
+                    Count = grp.Select(m => m.Title).Distinct().Count()
+                };
+
+            var genres = new List<string>();
+            var counts = new List<int>();
+
+            foreach (var row in query.OrderBy(m => m.Genre))
+            {
+                genres.Add(row.Genre);
+                counts.Add(row.Count);
+            }
+
+            return GetChart(genres, counts);
+        }
+
+        public ActionResult ChartByRating()
+        {
+            var query = from m in db.Movies
+                        group m by m.Rating
+                            into grp
+                            select new
+                            {
+                                Rating = grp.Key,
+                                Count = grp.Select(m => m.Title).Distinct().Count()
+                            };
+
+            var ratings = new List<string>();
+            var counts = new List<int>();
+
+            foreach (var row in query.OrderBy(m => m.Rating))
+            {
+                ratings.Add(row.Rating);
+                counts.Add(row.Count);
+            }
+
+            return GetChart(ratings, counts);
+        }
+    }
+}
---
+++ MvcMovie/readme.md
@@ -0,0 +1,11 @@
+This project is an example of a simple ASP.NET MVC application and is based on the following tutorials:
+
+	http://www.asp.net/mvc/tutorials/mvc-5/introduction/getting-started
+	http://www.windowsazure.com/en-us/documentation/articles/web-sites-dotnet-deploy-aspnet-mvc-app-membership-oauth-sql-database/
+
+After opening this project, build it, then open the Package Manager Console (under Tools, Library Package Manager) and run the following commands:
+
+	Add-Migration Initial
+	update-database
+
+Then hit F5 to run.
